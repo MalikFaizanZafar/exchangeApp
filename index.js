7c93131715c89ce8dcfed380973b1887da4de783
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-13 16:10:47 +0500

    added first browser window

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const electron = require('electron')
 
-const { app } = electron
+const { app, BrowserWindow } = electron
 
 app.on('ready', ()=> {
-  console.log("First Electron App is Running")
+  console.log("First Electron App has Started Running")
+  new BrowserWindow({})
+})
+
+app.on('quit', ()=> {
+  console.log("App has been Closed")
 })
