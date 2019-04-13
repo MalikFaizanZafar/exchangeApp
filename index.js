cea057712fc5b682ebc39821aa995d56c5a3c0f6
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-13 18:53:04 +0500

    added react to the project

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-const electron = require('electron')
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
-const { app, BrowserWindow } = electron
+const { app, BrowserWindow, ipcMain, shell } = electron;
 
-app.on('ready', ()=> {
-  console.log("First Electron App has Started Running")
-  const HomeWindow = new BrowserWindow({})
-  // HomeWindow.loadURL(`https://freecoursesite.com/`)
-  HomeWindow.loadURL(`file://${__dirname}/index.html`)
-})
+let mainWindow;
 
-app.on('quit', ()=> {
-  console.log("App has been Closed")
-})
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: { backgroundThrottling: false }
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
+    });
+  });
+
+  Promise.all(promises)
+    .then((results) => {
+      mainWindow.webContents.send('metadata:complete', results);
+    });
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+  _.each(videos, video => {
+    const outputDirectory = video.path.split(video.name)[0];
+    const outputName = video.name.split('.')[0]
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+
+    ffmpeg(video.path)
+      .output(outputPath)
+      .on('progress', ({ timemark }) =>
+        mainWindow.webContents.send('conversion:progress', { video, timemark })
+      )
+      .on('end', () =>
+        mainWindow.webContents.send('conversion:end', { video, outputPath })
+      )
+      .run();
+  });
+});
+
+ipcMain.on('folder:open', (event, outputPath) => {
+  shell.showItemInFolder(outputPath);
+});
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './reducers';
+
+import VideoSelectScreen from './screens/VideoSelectScreen';
+import ConvertScreen from './screens/ConvertScreen';
+
+ReactDOM.render(
+  <Provider store={store}>
+    {/* <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/convert" component={ConvertScreen} />
+          <Route path="/" component={VideoSelectScreen} />
+        </Switch>
+      </div>
+    </Router> */}
+    <div>
+      Welcome to Electron-React App
+    </div>
+  </Provider>,
+  document.getElementById('root')
+);
