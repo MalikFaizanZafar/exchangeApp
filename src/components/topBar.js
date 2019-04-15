6e0c0e96582d1849205d2728a2ab436288f899e6
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-15 20:50:01 +0500

    imports working now

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = {
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
     marginLeft: -18,
-    marginRight: 10
-  }
+    marginRight: 10,
+  },
 };
-class topBar extends Component {
+
+class TopBar extends Component {
   render() {
     return (
       <div>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <IconButton
-              className="menu"
-              color="inherit"
-              aria-label="Menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit">
-              Photos
-            </Typography>
-          </Toolbar>
-        </AppBar>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton className="menu" color="inherit" aria-label="Exchange App">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Exchange App
+          </Typography>
+        </Toolbar>
+      </AppBar>
       </div>
-    );
+    )
   }
 }
-export default withStyles(styles)(topBar);
+
+const TopNavBar = withStyles(styles)(TopBar);
+export default TopNavBar;
--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -1,39 +1,15 @@
 import React, { Component } from 'react'
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -18,
-    marginRight: 10,
-  },
-};
+import TopNavBar from '../components/topBar';
 
 class MainPage extends Component {
   render() {
     return (
       <div>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton className="menu" color="inherit" aria-label="Exchange App">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            Exchange App
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <TopNavBar></TopNavBar>
         This is the MainPage
       </div>
     )
   }
 }
 
-export default withStyles(styles)(MainPage);
+export default MainPage;
