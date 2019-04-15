3a48f52629c5b2186b629f768b583ea84f124915
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-15 22:01:58 +0500

    added table

---
+++ src/components/mainPageTable.js
@@ -0,0 +1,684 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const styles = theme => ({
+  root: {
+    background: 'linear-gradient(#FE6B8B , #FF8E53)',
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+});
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+const rows = [{
+  "id": 1,
+  "name": "Fernandina Tomkys",
+  "city": "Frei",
+  "age": 18,
+  "class": 3
+}, {
+  "id": 2,
+  "name": "Adolphe Shimoni",
+  "city": "Santai",
+  "age": 21,
+  "class": 3
+}, {
+  "id": 3,
+  "name": "Dan Pedrozzi",
+  "city": "Eloúnda",
+  "age": 18,
+  "class": 7
+}, {
+  "id": 4,
+  "name": "Rustie Grigori",
+  "city": "Jinji",
+  "age": 14,
+  "class": 4
+}, {
+  "id": 5,
+  "name": "Curt Dobbin",
+  "city": "Resplendor",
+  "age": 26,
+  "class": 4
+}, {
+  "id": 6,
+  "name": "Lotti Fairbrass",
+  "city": "Rushanzhai",
+  "age": 22,
+  "class": 2
+}, {
+  "id": 7,
+  "name": "Gabriel Godby",
+  "city": "Suren",
+  "age": 21,
+  "class": 4
+}, {
+  "id": 8,
+  "name": "Tova Warlton",
+  "city": "Dengyue",
+  "age": 18,
+  "class": 5
+}, {
+  "id": 9,
+  "name": "Marrilee Zolini",
+  "city": "Ngantru",
+  "age": 28,
+  "class": 5
+}, {
+  "id": 10,
+  "name": "Antonius Revie",
+  "city": "Ludvika",
+  "age": 10,
+  "class": 4
+}, {
+  "id": 11,
+  "name": "Jorge McParland",
+  "city": "Qorashina",
+  "age": 11,
+  "class": 6
+}, {
+  "id": 12,
+  "name": "Alyse Canet",
+  "city": "Karang Tengah",
+  "age": 30,
+  "class": 9
+}, {
+  "id": 13,
+  "name": "Rayner Hyland",
+  "city": "Toubao",
+  "age": 29,
+  "class": 10
+}, {
+  "id": 14,
+  "name": "Aloise Maccaddie",
+  "city": "Burhānuddin",
+  "age": 27,
+  "class": 3
+}, {
+  "id": 15,
+  "name": "Ozzy Cranefield",
+  "city": "Yinping",
+  "age": 14,
+  "class": 2
+}, {
+  "id": 16,
+  "name": "Arvie Legrave",
+  "city": "Setono",
+  "age": 25,
+  "class": 1
+}, {
+  "id": 17,
+  "name": "Marylynne Bathow",
+  "city": "Ratíškovice",
+  "age": 30,
+  "class": 3
+}, {
+  "id": 18,
+  "name": "Audry Pitherick",
+  "city": "Oehaunu",
+  "age": 13,
+  "class": 9
+}, {
+  "id": 19,
+  "name": "Lorin Dible",
+  "city": "Fergana",
+  "age": 13,
+  "class": 2
+}, {
+  "id": 20,
+  "name": "Aeriela Mountjoy",
+  "city": "Marabahan",
+  "age": 30,
+  "class": 5
+}, {
+  "id": 21,
+  "name": "Lorrie Low",
+  "city": "Lyon",
+  "age": 12,
+  "class": 9
+}, {
+  "id": 22,
+  "name": "Christiane Craik",
+  "city": "Shanghu",
+  "age": 19,
+  "class": 2
+}, {
+  "id": 23,
+  "name": "Fabe Siggens",
+  "city": "Oslo",
+  "age": 21,
+  "class": 10
+}, {
+  "id": 24,
+  "name": "Westbrooke Ridhole",
+  "city": "Leijiadian",
+  "age": 16,
+  "class": 3
+}, {
+  "id": 25,
+  "name": "Conny Ennals",
+  "city": "Taiyanghe",
+  "age": 26,
+  "class": 3
+}, {
+  "id": 26,
+  "name": "Essie Devenport",
+  "city": "Arrifes",
+  "age": 28,
+  "class": 5
+}, {
+  "id": 27,
+  "name": "Pacorro Godain",
+  "city": "Korisós",
+  "age": 12,
+  "class": 9
+}, {
+  "id": 28,
+  "name": "Marve Maddock",
+  "city": "Kemuning",
+  "age": 27,
+  "class": 1
+}, {
+  "id": 29,
+  "name": "Jessi Leel",
+  "city": "Salt Lake City",
+  "age": 13,
+  "class": 5
+}, {
+  "id": 30,
+  "name": "Althea Dust",
+  "city": "Göteborg",
+  "age": 14,
+  "class": 7
+}, {
+  "id": 31,
+  "name": "Percy Manville",
+  "city": "Biancheng",
+  "age": 26,
+  "class": 7
+}, {
+  "id": 32,
+  "name": "Candis Walisiak",
+  "city": "Lindavista",
+  "age": 21,
+  "class": 7
+}, {
+  "id": 33,
+  "name": "Marv Brittle",
+  "city": "Pasarkuok",
+  "age": 29,
+  "class": 6
+}, {
+  "id": 34,
+  "name": "Andras Dove",
+  "city": "Fengshan",
+  "age": 29,
+  "class": 10
+}, {
+  "id": 35,
+  "name": "Cello Besnard",
+  "city": "Tatsuno",
+  "age": 22,
+  "class": 6
+}, {
+  "id": 36,
+  "name": "Tito Found",
+  "city": "Wuyang",
+  "age": 27,
+  "class": 1
+}, {
+  "id": 37,
+  "name": "Fernandina Eschalette",
+  "city": "Tongquan",
+  "age": 25,
+  "class": 3
+}, {
+  "id": 38,
+  "name": "Pietra Knifton",
+  "city": "Savanna-la-Mar",
+  "age": 11,
+  "class": 8
+}, {
+  "id": 39,
+  "name": "Dare Regan",
+  "city": "Aguilares",
+  "age": 18,
+  "class": 4
+}, {
+  "id": 40,
+  "name": "Dara Thornton",
+  "city": "Biaoshan",
+  "age": 18,
+  "class": 2
+}, {
+  "id": 41,
+  "name": "Bevin Chipp",
+  "city": "Ngedhubasa",
+  "age": 12,
+  "class": 8
+}, {
+  "id": 42,
+  "name": "Keefer Reilinger",
+  "city": "Amirdzhan",
+  "age": 28,
+  "class": 2
+}, {
+  "id": 43,
+  "name": "Renee Archambault",
+  "city": "Alicante/Alacant",
+  "age": 27,
+  "class": 5
+}, {
+  "id": 44,
+  "name": "Latashia Burkman",
+  "city": "Chocz",
+  "age": 27,
+  "class": 1
+}, {
+  "id": 45,
+  "name": "Fanchette Jills",
+  "city": "Nicasio",
+  "age": 10,
+  "class": 4
+}, {
+  "id": 46,
+  "name": "Meredithe Battie",
+  "city": "Sarongan",
+  "age": 23,
+  "class": 9
+}, {
+  "id": 47,
+  "name": "Maible O' Gara",
+  "city": "Montauban",
+  "age": 10,
+  "class": 3
+}, {
+  "id": 48,
+  "name": "Concordia Michele",
+  "city": "Ma'an",
+  "age": 22,
+  "class": 7
+}, {
+  "id": 49,
+  "name": "Shayna Barkas",
+  "city": "Chirpan",
+  "age": 27,
+  "class": 10
+}, {
+  "id": 50,
+  "name": "Lindsay Orrow",
+  "city": "Swan Hills",
+  "age": 19,
+  "class": 4
+}, {
+  "id": 51,
+  "name": "Brockie Adicot",
+  "city": "Santa Catarina",
+  "age": 11,
+  "class": 1
+}, {
+  "id": 52,
+  "name": "Perice Beller",
+  "city": "Tabasalu",
+  "age": 24,
+  "class": 7
+}, {
+  "id": 53,
+  "name": "Meggi Broadhead",
+  "city": "Pira",
+  "age": 29,
+  "class": 10
+}, {
+  "id": 54,
+  "name": "Aretha Gumm",
+  "city": "Mansôa",
+  "age": 15,
+  "class": 8
+}, {
+  "id": 55,
+  "name": "Clyde Woodwind",
+  "city": "Taverny",
+  "age": 16,
+  "class": 8
+}, {
+  "id": 56,
+  "name": "Codie Cruise",
+  "city": "Sakerta Timur",
+  "age": 30,
+  "class": 5
+}, {
+  "id": 57,
+  "name": "Edlin Veldens",
+  "city": "Szlachta",
+  "age": 20,
+  "class": 6
+}, {
+  "id": 58,
+  "name": "Nonna MacAlpyne",
+  "city": "Souto do Meio",
+  "age": 14,
+  "class": 3
+}, {
+  "id": 59,
+  "name": "Sandy Bevir",
+  "city": "Siyang",
+  "age": 30,
+  "class": 4
+}, {
+  "id": 60,
+  "name": "Otto Ranns",
+  "city": "Caldono",
+  "age": 13,
+  "class": 7
+}, {
+  "id": 61,
+  "name": "Blisse Mosby",
+  "city": "Aoji-ri",
+  "age": 12,
+  "class": 8
+}, {
+  "id": 62,
+  "name": "Illa Tallboy",
+  "city": "Sypniewo",
+  "age": 11,
+  "class": 9
+}, {
+  "id": 63,
+  "name": "Sanderson Salliere",
+  "city": "Bagenalstown",
+  "age": 28,
+  "class": 6
+}, {
+  "id": 64,
+  "name": "Iseabal Oluwatoyin",
+  "city": "Bacacay",
+  "age": 14,
+  "class": 10
+}, {
+  "id": 65,
+  "name": "Damaris Mattioni",
+  "city": "San Francisco",
+  "age": 21,
+  "class": 8
+}, {
+  "id": 66,
+  "name": "Kathryne Dono",
+  "city": "Ceará Mirim",
+  "age": 23,
+  "class": 8
+}, {
+  "id": 67,
+  "name": "Cahra Heel",
+  "city": "Damu",
+  "age": 27,
+  "class": 9
+}, {
+  "id": 68,
+  "name": "Sharline Demangeon",
+  "city": "Damāvand",
+  "age": 25,
+  "class": 10
+}, {
+  "id": 69,
+  "name": "Barnaby Mion",
+  "city": "Xubao",
+  "age": 24,
+  "class": 6
+}, {
+  "id": 70,
+  "name": "Eunice Solomonides",
+  "city": "Lýkeio",
+  "age": 30,
+  "class": 2
+}, {
+  "id": 71,
+  "name": "Marcelia Battyll",
+  "city": "Wenceslau Braz",
+  "age": 21,
+  "class": 3
+}, {
+  "id": 72,
+  "name": "Isis Blaisdale",
+  "city": "Pedroso",
+  "age": 19,
+  "class": 1
+}, {
+  "id": 73,
+  "name": "Alleyn Kybert",
+  "city": "Yanqian",
+  "age": 17,
+  "class": 2
+}, {
+  "id": 74,
+  "name": "Pamelina Larroway",
+  "city": "Timbó",
+  "age": 17,
+  "class": 8
+}, {
+  "id": 75,
+  "name": "Mandy Mangeot",
+  "city": "Teresina",
+  "age": 20,
+  "class": 7
+}, {
+  "id": 76,
+  "name": "Hilliard Rasell",
+  "city": "Noginsk-9",
+  "age": 12,
+  "class": 10
+}, {
+  "id": 77,
+  "name": "Gina Biffin",
+  "city": "Dianshui",
+  "age": 18,
+  "class": 10
+}, {
+  "id": 78,
+  "name": "Edgardo Lindblom",
+  "city": "Linton",
+  "age": 12,
+  "class": 5
+}, {
+  "id": 79,
+  "name": "Tull Krammer",
+  "city": "Pýli",
+  "age": 15,
+  "class": 4
+}, {
+  "id": 80,
+  "name": "Ivette Jouhning",
+  "city": "E’erdun Wula",
+  "age": 10,
+  "class": 3
+}, {
+  "id": 81,
+  "name": "Roscoe Kment",
+  "city": "Bandeirantes",
+  "age": 20,
+  "class": 7
+}, {
+  "id": 82,
+  "name": "Nelie Worthington",
+  "city": "Plottier",
+  "age": 27,
+  "class": 1
+}, {
+  "id": 83,
+  "name": "Sasha Fittes",
+  "city": "Jianling",
+  "age": 28,
+  "class": 10
+}, {
+  "id": 84,
+  "name": "Penn Woodrup",
+  "city": "Tsaratanana",
+  "age": 29,
+  "class": 5
+}, {
+  "id": 85,
+  "name": "Kathi Enticott",
+  "city": "Chlumčany",
+  "age": 17,
+  "class": 8
+}, {
+  "id": 86,
+  "name": "Emera Honniebal",
+  "city": "Isehara",
+  "age": 27,
+  "class": 9
+}, {
+  "id": 87,
+  "name": "Courtney Bown",
+  "city": "Drammen",
+  "age": 17,
+  "class": 6
+}, {
+  "id": 88,
+  "name": "Jack Gemson",
+  "city": "Syrskoye",
+  "age": 17,
+  "class": 4
+}, {
+  "id": 89,
+  "name": "Boonie Shillington",
+  "city": "Kaltungo",
+  "age": 17,
+  "class": 3
+}, {
+  "id": 90,
+  "name": "Clarance Toulson",
+  "city": "Santa Marcela",
+  "age": 18,
+  "class": 10
+}, {
+  "id": 91,
+  "name": "Vlad Channon",
+  "city": "Tres Isletas",
+  "age": 20,
+  "class": 7
+}, {
+  "id": 92,
+  "name": "Kandace Bridgnell",
+  "city": "Berkovitsa",
+  "age": 18,
+  "class": 8
+}, {
+  "id": 93,
+  "name": "Rickey Higginbottam",
+  "city": "Omboué",
+  "age": 26,
+  "class": 4
+}, {
+  "id": 94,
+  "name": "Kent Winward",
+  "city": "Pumaqangtang",
+  "age": 12,
+  "class": 5
+}, {
+  "id": 95,
+  "name": "Melisa Bonicelli",
+  "city": "Eastern Suburbs Mc",
+  "age": 28,
+  "class": 2
+}, {
+  "id": 96,
+  "name": "Gwennie Rorke",
+  "city": "Şabrātah",
+  "age": 14,
+  "class": 4
+}, {
+  "id": 97,
+  "name": "Carlynne Gosling",
+  "city": "Goundam",
+  "age": 15,
+  "class": 5
+}, {
+  "id": 98,
+  "name": "Abelard Pover",
+  "city": "Grenoble",
+  "age": 25,
+  "class": 6
+}, {
+  "id": 99,
+  "name": "Ricky Rumbellow",
+  "city": "Barice",
+  "age": 22,
+  "class": 3
+}, {
+  "id": 100,
+  "name": "Gunner Goghin",
+  "city": "Cupira",
+  "age": 17,
+  "class": 5
+}]
+
+function CustomizedTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead style={{ backgroundColor : 'linear-gradient(#FE6B8B , #FF8E53)'}}>
+          <TableRow>
+            <CustomTableCell>Reg No.</CustomTableCell>
+            <CustomTableCell align="right">Name</CustomTableCell>
+            <CustomTableCell align="right">Age</CustomTableCell>
+            <CustomTableCell align="right">Class</CustomTableCell>
+            <CustomTableCell align="right">City</CustomTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow className={classes.row} key={row.id}>
+              <CustomTableCell component="th" scope="row">
+                {row.id}
+              </CustomTableCell>
+              <CustomTableCell align="right">{row.name}</CustomTableCell>
+              <CustomTableCell align="right">{row.age}</CustomTableCell>
+              <CustomTableCell align="right">{row.class}</CustomTableCell>
+              <CustomTableCell align="right">{row.city}</CustomTableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+CustomizedTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const MainPageTable = withStyles(styles)(CustomizedTable);
+export default MainPageTable;
--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import TopNavBar from '../components/topBar';
+import MainPageTable from '../components/mainPageTable';
+import Grid from '@material-ui/core/Grid';
 
 class MainPage extends Component {
   render() {
     return (
       <div>
       <TopNavBar></TopNavBar>
-        This is the MainPage
+      <Grid container spacing={12} justify="center">
+      <Grid item sm={8}>
+        <MainPageTable></MainPageTable>
+      </Grid>
+      </Grid>
       </div>
     )
   }
