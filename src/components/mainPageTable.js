8c4e46c9c748d6eb87cdbd83035bfef540770dad
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-20 18:57:17 +0500

    added extra fields in table data

--- src/components/mainPageTable.js
+++ src/components/mainPageTable.js
@@ -10,7 +10,6 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
-import Icon from '@material-ui/core/Icon';
 import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
